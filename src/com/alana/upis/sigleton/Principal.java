d05f89fd10010da242c89076fe769ee394f8507b
alanapaulamota <alanapaula.job@gmail.com>
2020-04-27 21:54:29 -0300

    Iniciando dever de Singleton...

---
+++ src/com/alana/upis/sigleton/Principal.java
@@ -0,0 +1,14 @@
+package com.alana.upis.sigleton;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		System.out.println("******** Exercicio de Singleton *******\n");
+		System.out.println("******** Relógio ********");
+		System.out.println("Data: "+Relogio.getInstancia().getData());
+		System.out.println("Hora: "+Relogio.getInstancia().getHorario());
+//		Relogio.getInstancia().Horario();
+	}
+
+}
